94cc83cb1d0248fe790d541b4ba73024de715bf4
Mcvdieu <Mathiascvdieu@hotmail.com>
2017-05-18 21:09:37 +0200

    mobile navbar

--- EsportProject/wwwroot/js/site.js
+++ EsportProject/wwwroot/js/site.js
@@ -1,1 +1,9 @@
-﻿// Write your Javascript code.
+﻿$(document).ready(function() {
+
+    $(".burger-nav").on("click", function () {
+
+        $("header nav ul").toggleClass("open");
+    });
+
+
+});
