d71bb4d697771868842f4e4c94388d3158b2626c
Thomas Crone Larsen <thom802b@edu.eal.dk>
2017-05-26 11:42:24 +0200

    News controller added, and works (I think?)

---
+++ EsportProject/Models/DBmodels/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EsportProject.Models.DBmodels
+{
+    public class Class
+    {
+    }
+}
