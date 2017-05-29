2de35d60e56e15134881b75b63309630db170c70
newnamtab <newnamtab@yahoo.dk>
2017-05-29 13:46:12 +0200

    Contact part added with a bit of XXS and HTML injection prevention

---
+++ EsportProject/Classes/Sanitizer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EsportProject.Classes
+{
+    public static class Sanitizer
+    {
+        public static string SanitizeText(string stringToSanitize)
+        {
+
+            string sanitizedString = System.Text.RegularExpressions.Regex.Replace(stringToSanitize, "<script>", "", System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            
+
+            return sanitizedString;
+        }
+    }
+}
