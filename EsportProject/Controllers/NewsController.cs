7e4ceb597ea45fc9f471d94480f6e8bdab79d593
Crone <crone1992@gmail.com>
2017-05-29 12:44:31 +0200

    Added upload image functionality to Create news.

--- EsportProject/Controllers/NewsController.cs
+++ EsportProject/Controllers/NewsController.cs
@@ -8,17 +8,19 @@ using Microsoft.EntityFrameworkCore;
 using EsportProject.Models.DBmodels;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
 
 namespace EsportProject.Controllers
 {
     public class NewsController : Controller
     {
         private readonly NewsContext _context;
-
+        private IHostingEnvironment _environment;
         public NewsController(NewsContext context, IHostingEnvironment environment)
         {
             _context = context;
-
+            _environment = environment;
         }
 
         // GET: News
@@ -56,8 +58,23 @@ namespace EsportProject.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("NewsID,Title,subtitle,Content,imgURL,CreateDate")] News news)
+        public async Task<IActionResult> Create([Bind("NewsID,Title,subtitle,Content,imgURL,CreateDate")] News news, IFormFile file)
         {
+            //Code to upload IMG
+            long size = 0;
+            var filename = ContentDispositionHeaderValue
+                               .Parse(file.ContentDisposition)
+                               .FileName
+                               .Trim('"');
+            news.imgURL = $@"\wwwroot\images\" + filename; //Sets the path of the img to the news object
+            filename = _environment.WebRootPath + $@"\images\" + filename;
+            size += file.Length;
+            using (FileStream fs = System.IO.File.Create(filename))
+            {
+                file.CopyTo(fs);
+                fs.Flush();
+            }
+            //Code to update DB
             if (ModelState.IsValid)
             {
                 _context.Add(news);
@@ -152,6 +169,27 @@ namespace EsportProject.Controllers
         private bool NewsExists(int id)
         {
             return _context.News.Any(e => e.NewsID == id);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Upload(IList<IFormFile> files)
+        {
+            long size = 0;
+            foreach (var file in files)
+            {
+                var filename = ContentDispositionHeaderValue
+                                .Parse(file.ContentDisposition)
+                                .FileName
+                                .Trim('"');
+                filename = _environment.WebRootPath + $@"\images\" + filename;
+                size += file.Length;
+                using (FileStream fs = System.IO.File.Create(filename))
+                {
+                    file.CopyTo(fs);
+                    fs.Flush();
+                }
+            }
+            ViewBag.Message = $"file" + size + " bytes uploaded successfully!";
+            return RedirectToAction("Index");
         }
     }
 }
