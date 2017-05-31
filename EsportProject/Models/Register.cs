4f2ed93dfc82094218979e1dd0413cdd3bf419be
MCVDIEU <mathiascvdieu@hotmail.com>
2017-05-31 11:00:01 +0200

    Register user

---
+++ EsportProject/Models/Register.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EsportProject.Models
+{
+    public class Register
+    {
+        [Required, EmailAddress, MaxLength(256), Display(Name = "Email Address")]
+        public string Email { get; set; }
+        [Required, MinLength(6),MaxLength(50),DataType(DataType.Password),Display(Name = "Password")]
+        public string Password { get; set; }
+        [Required, MinLength(6), MaxLength(50), DataType(DataType.Password), Display(Name = "Confirm Password")]
+        
+        public string ConfirmPassword { get; set; }
+
+    }
+}
