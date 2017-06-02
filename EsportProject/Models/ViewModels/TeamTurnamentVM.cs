a54c75145cdc587f5cfe2c60c4aa3b3da5069725
Thomas Crone Larsen <thom802b@edu.eal.dk>
2017-06-02 10:25:18 +0200

    Changes to HomeController with focus on tournament.

---
+++ EsportProject/Models/ViewModels/TeamTurnamentVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EsportProject.Models.DBmodels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EsportProject.Models
+{
+    public class TeamTurnamentVM
+    {
+        private readonly TurnamentContext _context;
+        public List<SelectListItem> TeamsList { get; set; }
+        public List<SelectListItem> TurnamentList;
+
+        public TeamTurnamentVM(TurnamentContext context)
+        {
+            _context = context;
+            //List<Team> teamList = _context.Team.ToList();
+            List<Team> teamlist = _context.Team.ToList();
+            List<Turnament> turnamentList = _context.Turnament.ToList();
+            SetLists(turnamentList, teamlist);
+        }
+        public void SetLists(List<Turnament> turns, List<Team> teams)
+        {
+            TurnamentList = new List<SelectListItem>();
+            TeamsList = new List<SelectListItem>();
+            foreach (Team tm in teams)
+            {
+               TeamsList.Add(new SelectListItem { Value = tm.TeamID.ToString(), Text = tm.Name });
+            }
+            foreach (Turnament tur in turns)
+            {
+                TurnamentList.Add(new SelectListItem { Value = tur.TurnamentID.ToString(), Text = tur.Name });
+            }
+        }
+    }
+}
---
+++ EsportProject/Models/ViewModels/TournamentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EsportProject.Models.DBmodels;
+
+namespace EsportProject.Models
+{
+    public class TournamentViewModel
+    {
+        public Turnament tournament { get; set; }
+        public List<TeamPointsVM> TeamList { get; set; }
+        public TournamentViewModel(Turnament tour)
+        {
+            tournament = tour;
+            TeamList = new List<TeamPointsVM>();
+        }
+    }
+}
