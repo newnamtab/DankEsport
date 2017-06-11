1f9f16a486041cf6a910d17bd5b869cdf616f5d8
Mcvdieu <Mathiascvdieu@hotmail.com>
2017-06-11 19:54:57 +0200

    tilføjet JqueryValidation eksempel

--- EsportProject/wwwroot/js/site.js
+++ EsportProject/wwwroot/js/site.js
@@ -7,3 +7,30 @@
 
 
 });
+
+//JqueryValidation på register form
+//$("regcon").validate({
+//    rules: {
+//        email: {
+//            required: true, //Feltet er required
+//            email: true //Feltet skal have en email
+//        },
+//        password: 'required', //påkrævet field
+//        confirmpassword: {
+//            required: true, //påkrævet field
+//            equalTo: ''//Vil ramme name: password (sammenlignes efter med det password ovenover)
+//        }
+//    },
+//    messages: {
+//        email:{
+//            required: 'Please enter an email', //Custom beskeder til felterne
+//            email: 'Please enter a valid email' //Custom beskeder til felterne
+//        }
+//    }
+//});
+
+//JavaScript metode
+//function TestFunction() {
+//    // Funktionalitet
+//    document.getElementById("demo").innerHTML = "Hello world";
+//}
